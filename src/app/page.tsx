e940a5b84fda9c2503f052f686cdfd01558513e8
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-05-16 17:27:46 -0700

    add Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return  <h1>Welcome to e-lib client app</h1> ;
+  return (
+    <>
+    <h1>welcome to elibrary</h1>
+  </>
+  );
 }
